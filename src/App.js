5fc4b90386ba4f9d9925b528d9b611fe39c26c4f
juslesan <santeri.juslenius@gmail.com>
2021-06-29 13:31:26 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { hri } from 'human-readable-ids'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import { startNetworkNode } from 'streamr-network'
+
+class App extends React.Component {
+  state = {
+    id: hri.random(),
+    state: 'Not Connected'
+  }
+
+  render() {
+    return (
+      <div className="Wrapper">
+        <h1>Streamr Network Node Browser Test!</h1>
+        <p> Your Browser Node ID:</p>
+        <p style={{fontWeight: "bold"}}> { this.state.id }</p>
+        <p style={{marginTop: "50px"}}> Num of connections: { this.state.connections }</p>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
